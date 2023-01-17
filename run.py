b2d4199049b1614d56416bdac0976e316510d133
danilakovtunn <danila.kovtunn@gmail.com>
2023-01-17 15:21:35 +0300

    add parsing arguments

--- run.py
+++ run.py
@@ -1,30 +1,78 @@
 import subprocess
 import datetime
 import os
+import sys
+import argparse
 
 
-start = datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
-pipe = subprocess.Popen(
-    ('sbt', 
-    '-Djaas_url=10.100.203.110', 
-    '-Dport=5000', 
-    '-Dprotocol=http',
-    '-Dusers=5',
-    '-Dramp=30', 
-    'Gatling/test'), 
-    stdout=subprocess.PIPE
-)
-x = pipe.stdout.readlines()
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--jaas_ip',
+        type=str,
+        default='10.100.203.110',
+        help='ip address where Jupyter As A Servise is located'
+    )
+    parser.add_argument(
+        '--port',
+        type=int,
+        default=5000,
+        help='port to connect to JAAS'
+    )
+    parser.add_argument(
+        '--protocol',
+        type=str,
+        choices=['http', 'https'],
+        default='http',
+        help='protocol to JAAS'
+    )
+    parser.add_argument(
+        '--users',
+        type=int,
+        default=5,
+        help='number of virtual users that will load the server'
+    )
+    parser.add_argument(
+        '--ramp',
+        type=int,
+        default=0,
+        help='given number of users distributed evenly on a time window of a given ramp'
+    )
 
-for i in x:
-    print(i.decode(), end='')
+    args, args_list = parser.parse_known_args(argv)
+    return args.jaas_ip, args.port, args.protocol, args.users, args.ramp
 
-if len(x[-8].split()) < 3 or x[-8].split()[2] != b'0':
-    finish = datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
-    error_files = list(filter(lambda x: x[:-4].isdecimal() and x[-4:] == '.txt', os.listdir('datetime_errors')))
-    number = len(error_files) + 1
-    with open('datetime_errors/' + str(number) + '.txt', 'w') as f:
-        for i in x:
-            f.write(i.decode())
-    with open('datetime_errors/errors.txt', 'a') as f:
-        f.write(str(number) + ': ' + start + '     -     ' + finish + '\n')
+
+def main():
+    argv = sys.argv[1:]
+    jaas_url, port, protocol, users, ramp = parse_args(argv)
+
+    start = datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
+    pipe = subprocess.Popen(
+        ('sbt', 
+        '-Djaas_url=' + jaas_url, 
+        '-Dport=' + str(port), 
+        '-Dprotocol=' + protocol,
+        '-Dusers=' + str(users),
+        '-Dramp=' + str(ramp), 
+        'Gatling/test'), 
+        stdout=subprocess.PIPE
+    )
+    x = pipe.stdout.readlines()
+
+    for i in x:
+        print(i.decode(), end='')
+
+    if len(x[-8].split()) < 3 or x[-8].split()[2] != b'0':
+        finish = datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
+        error_files = list(filter(lambda x: x[:-4].isdecimal() and x[-4:] == '.txt', os.listdir('datetime_errors')))
+        number = len(error_files) + 1
+        with open('datetime_errors/' + str(number) + '.txt', 'w') as f:
+            for i in x:
+                f.write(i.decode())
+        with open('datetime_errors/errors.txt', 'a') as f:
+            f.write(str(number) + ': ' + start + '     -     ' + finish + '\n')
+
+
+if __name__ == '__main__':
+    main()
